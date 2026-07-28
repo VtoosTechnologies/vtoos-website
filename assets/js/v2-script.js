fd04cb7453f35b7d24527dd1c0104ffe59d69e14
VtoosTechnologies <vtoos.software@gmail.com>
2026-07-28 23:31:38 +0530

    Add scroll-to-top button with smooth scrolling
    Implement scroll-to-top button functionality.

--- assets/js/v2-script.js
+++ assets/js/v2-script.js
@@ -254,3 +254,35 @@ console.log(
 "color:#555;font-size:14px;"
 
 );
+/*====================================
+Scroll To Top
+====================================*/
+
+const scrollBtn =
+document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll",()=>{
+
+if(window.scrollY>500){
+
+scrollBtn.style.display="flex";
+
+}else{
+
+scrollBtn.style.display="none";
+
+}
+
+});
+
+scrollBtn.addEventListener("click",()=>{
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+});
